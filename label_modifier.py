b63a13bce3518e8ddcf694ba894bf21d4dd0ec66
Tanush Dhamija <tanush@v3smarttech.com>
2024-03-19 15:29:20 +0530

    add: new file to quickly replace class_id of YOLO .txt files

---
+++ label_modifier.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+def modify_label(folder, old_label, new_label):
+
+    labels = [os.path.join(folder,label) for label in os.listdir(folder) if label != 'classes.txt']
+
+    print(f'changing class_id from {old_label} to {new_label} for {len(labels)} files...')
+    for label in labels:
+        # read file
+        with open(label,'r') as file:
+            lines = file.readlines()
+
+        # replace labels
+        newlines = []
+        for line in lines:
+            # line = line.strip()
+            class_id = line.split(" ")[0]
+            if class_id == old_label:
+                new_line = new_label + " " + " ".join(line.split(" ")[1:])
+            else:
+                new_line = line
+            newlines.append(new_line)
+
+        # write back to file
+        with open(label,'w') as file:
+            file.writelines(newlines)
+
+    print('\ndone.')
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(f"usage: python {sys.argv[0]} </path/to/labels> <class_id to replace> <new class_id>")
+
+    modify_label(*sys.argv[1:])
